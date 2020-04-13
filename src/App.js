2c65a9dac05334bb14870aa73d52700e227521c5
Demetrius Araujo <demetriusegundo@gmail.com>
2020-04-13 00:12:28 -0300

    Finalização do Desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { FiThumbsUp } from 'react-icons/fi';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRespositories] = useState([]);
+
+  useEffect(()=>{
+    api.get('/repositories').then(repository=>{
+      setRespositories(repository.data);
+    }
+    );
+  }, []);
   async function handleAddRepository() {
-    // TODO
+    const result = await api.post('/repositories',{
+      title:"Desafio ReactJS",
+      url:"github.com/DemetriusADS",
+      techs:["nodejs","reactjs"]
+    });
+
+    const repository = result.data;
+
+    setRespositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    const newRepo = repositories.filter(repository => repository.id !== id);
+
+    setRespositories(newRepo);
+  }
+
+  async function handleLikesRepository(id){
+    const liked = await api.post(`/repositories/${id}/like`);
+
+    const likedIndex = repositories.findIndex(repository => repository.id === id);
+    
+    const refreshLikes = [...repositories];
+
+    refreshLikes[likedIndex].likes = liked.data.likes;
+
+    setRespositories(refreshLikes);
   }
 
   return (
-    <div>
+    <div className="container">
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository)=>
+            <li key={repository.id}>
+              <div className="container-inner">
+                <h3>Titulo: {repository.title}</h3>
+                <h3>Url: <a href={repository.url}>{repository.url}</a></h3>
+                <h3>Techs: {repository.techs.map(techs=>`#${techs} `)}</h3>
+                <div className="likes">
+                  <button className="likeButton" onClick={()=>handleLikesRepository(repository.id)}>
+                    <FiThumbsUp size={15}/>
+                  </button>
+                  <span>{repository.likes}</span>
+                </div>
+                
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </div>
+            </li>
+        )}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
